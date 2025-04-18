e2aeee43d428e567922cb7fb8525e3442b78e1ce
Bim Paras <bimp@bimparas.com>
2025-04-18 16:09:34 -0700

    added echo install statement

--- install-scripts/bat-install.sh
+++ install-scripts/bat-install.sh
@@ -4,6 +4,7 @@ set -o errexit # exits if error occurs
 set -o nounset # fails when accessing an unset variable
 set -o pipefail # pipeline command is treated as failed, even if one command in the pipeline fail
 
+echo "Installing bat..."
 sudo -u root apt install -y bat
 
 # the executable may be installed as batcat instead of bat (due to a name clash with another package). 
