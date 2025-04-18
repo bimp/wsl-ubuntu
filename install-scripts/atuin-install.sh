0d3e407db027dabf5a171d8383e466f6eedd3e5c
Bim Paras <bimp@bimparas.com>
2025-04-18 16:10:42 -0700

    added atuin pre existence check and version output

--- install-scripts/atuin-install.sh
+++ install-scripts/atuin-install.sh
@@ -8,4 +8,12 @@ set -o pipefail # pipeline command is treated as failed, even if one command in 
 # https://atuin.sh
 # This script installs atuin using the official installation script.
 echo "Installing atuin..."
-sudo -u bimp curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
+# install only if ~/.atuin/bin/atuin does not exist
+if [ -f ~/.atuin/bin/atuin ]; then
+    echo "atuin already installed. skipping installation."
+else
+    sudo -u bimp curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
+fi
+
+printf "\natuin version:\n"
+~/.atuin/bin/atuin --version
